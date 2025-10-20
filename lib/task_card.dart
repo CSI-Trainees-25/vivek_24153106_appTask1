64702d115b4835a03554966f69efcd0ea5de4ec4
Vivek Yadav <vivekyadav00681@gmail.com>
2025-10-20 20:44:57 +0530

    updated app UI added basic calendar table

--- lib/task_card.dart
+++ lib/task_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app_csi/formatted_text.dart';
 import 'package:todo_app_csi/task.dart';
 
 class TaskCard extends StatefulWidget {
@@ -15,33 +16,55 @@ class _TaskCardState extends State<TaskCard> {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-        child: Column(
-          children: [
-            Text(widget.task.title),
-            const SizedBox(height: 5),
-            Row(
-              children: [
-                const Spacer(),
-                Row(
-                  children: [
-                    Checkbox(
-                      value: widget.task.isDone,
-                      onChanged: (newValue) {
-                        setState(() {
-                          widget.task.isDone = newValue;
-                        });
-                      },
-                    ),
-                    Icon(taskCategory[widget.task.category]),
-                    SizedBox(width: 50),
-                    Text(widget.task.FormattedDate),
-                  ],
-                ),
-              ],
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black26,
+              blurRadius: 8,
+              offset: Offset(2, 2),
             ),
           ],
+          gradient: LinearGradient(
+            colors: [
+              const Color.fromARGB(255, 49, 49, 49),
+              const Color.fromARGB(255, 66, 66, 66),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+          child: Column(
+            children: [
+              FormattedText(widget.task.title),
+              const SizedBox(height: 5),
+              Row(
+                children: [
+                  const Spacer(),
+                  Row(
+                    children: [
+                      Icon(taskCategory[widget.task.category]),
+                      Checkbox(
+                        value: widget.task.isDone,
+                        onChanged: (newValue) {
+                          setState(() {
+                            widget.task.isDone = newValue;
+                          });
+                        },
+                      ),
+
+                      SizedBox(width: 200),
+                      FormattedText(widget.task.FormattedDate),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/tasks.dart
+++ lib/tasks.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:todo_app_csi/task_list.dart';
 import 'package:todo_app_csi/task.dart';
 import 'package:todo_app_csi/new_task.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class Tasks extends StatefulWidget {
-  Tasks({super.key});
+  const Tasks({super.key});
 
   @override
   State<StatefulWidget> createState() {
@@ -17,7 +18,7 @@ class TasksState extends State<Tasks> {
     Task(
       title: 'Create this todo app for CSI task 4',
       category: Category.society,
-      date: new DateTime.now(),
+      date: DateTime.now(),
     ),
     Task(
       title: 'Complete reading that novel.',
@@ -31,6 +32,12 @@ class TasksState extends State<Tasks> {
       context: context,
       builder: (ctx) => NewTask(onAddTask: onAddTask),
     );
+  }
+
+  int getIndex() {
+    int index = 0;
+    if (index >= _registeredTasks.length) index = 0;
+    return index++;
   }
 
   void onAddTask(Task task) {
@@ -64,17 +71,67 @@ class TasksState extends State<Tasks> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 39, 38, 38),
       appBar: AppBar(
-        shadowColor: Colors.black,
-        title: const Text('To Do App'),
-        backgroundColor: Colors.amber,
+        backgroundColor: const Color.fromARGB(255, 82, 81, 81),
+        title: const Text(
+          'To Do App',
+          style: TextStyle(color: Colors.white, fontSize: 20),
+        ),
         actions: [
           IconButton(onPressed: _openModalOverlay, icon: const Icon(Icons.add)),
         ],
       ),
       body: Column(
         children: [
-          const Text('Something'),
+          const Text(
+            'calendar view',
+            style: TextStyle(color: Colors.white, fontSize: 16),
+          ),
+          Card(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+              child: Column(
+                children: [
+                  Text('view calendar view of the tasks'),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black26,
+                          blurRadius: 8,
+                          offset: Offset(2, 2),
+                        ),
+                      ],
+                      gradient: LinearGradient(
+                        colors: [
+                          const Color.fromARGB(255, 49, 49, 49),
+                          const Color.fromARGB(255, 66, 66, 66),
+                        ],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                      ),
+                    ),
+                    child: TableCalendar(
+                      focusedDay: _registeredTasks[getIndex()].date,
+                      firstDay: DateTime(
+                        _registeredTasks[getIndex()].date.year - 1,
+                      ),
+                      lastDay: DateTime(
+                        _registeredTasks[getIndex()].date.year + 1,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const Text(
+            'tasks',
+            style: TextStyle(color: Colors.white, fontSize: 16),
+          ),
           Expanded(
             child: TaskList(tasks: _registeredTasks, onRemove: onRemoveTask),
           ),
